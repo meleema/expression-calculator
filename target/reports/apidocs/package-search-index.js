05a8fe5e2874f36c292404b62e62d9d5e637db6f
meleema <maria_09.12.2005@mail.ru>
2025-11-11 22:02:49 +0300

    Configure .gitignore and remove target directory from tracking

--- target/reports/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.calculator"},{"l":"com.calculator.calculator"},{"l":"com.calculator.calculator.functions"},{"l":"com.calculator.exception"}];updateSearchResults();
